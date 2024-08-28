420c91cdb547d67e0408096f6af64e3ab5930634
Song <1335130740@qq.com>
2024-08-28 21:29:58 +0800

    feat(deploy): add file for deploying user rpc
    This commit introduces the initial deployment.
    - The docker-compose.yaml: defines the container image for the dependency.
    - The Makefile includes targets for building user rpc docker image.

---
+++ deploy/script/release-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+need_start_service_shell=(
+    # rpc
+    user-rpc-test.sh
+
+    # api
+)
+
+for i in ${need_start_service_shell[*]} ; do
+    chmod +x $i
+    ./$i
+done
+
+
+docker ps
+
+docker exec -it etcd etcdctl get --prefix ""
